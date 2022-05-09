12d4f76b1a2811d9551a1bdaf4737320e0ce5d64
imac <imac@iMac-imac-3.local>
2022-05-09 21:48:24 +0300

    first commit

---
+++ ToDoAppCommunity/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoAppCommunity
+//
+//  Created by imac on 09.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
