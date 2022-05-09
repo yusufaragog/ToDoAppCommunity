5cd29717b91cd8c3ee3c77770e29e0ad8e3ba239
yusufaragog <yusuf.atkaev@gmail.com>
2022-05-09 21:58:24 +0300

    print added

--- ToDoAppCommunity/ViewController.swift
+++ ToDoAppCommunity/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("ahsvdkasyvdasud")
         // Do any additional setup after loading the view.
     }
 
